89093de5a1c8cbec7c838d1490e63dadc30cb6dd
Walter Fritsche <wfritsche95@protonmail.com>
2022-08-03 15:50:35 -0500

    added all links and styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-left-links">
+          <ul>
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/">
+              <li>About</li>
+            </Link>
+            <Link to="/shop">
+              <li>Shop</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="footer-right-links">
+          <ul>
+            <Link to="/">
+              <li>Faq</li>
+            </Link>
+            <Link to="/">
+              <li>Privacy</li>
+            </Link>
+            <Link to="/">
+              <li>Green Pledge</li>
+            </Link>
+          </ul>
+        </div>
+        <div className="footer-bottom-center">
+          <p>Tree Shoppe, LLC 2022</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
