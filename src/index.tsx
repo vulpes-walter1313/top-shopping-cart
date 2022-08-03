bb34fbec374b34860f0c726408ed42c87a27892b
Walter Fritsche <wfritsche95@protonmail.com>
2022-08-03 15:48:32 -0500

    added route shop

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './routes/Shop';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}/>
+      <Route path="shop" element={<Shop />}/>
+    </Routes>
   </BrowserRouter>
 );
 
