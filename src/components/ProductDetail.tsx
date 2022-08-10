b68ff427bfb8947605ef2c43be5b57f852e76670
Walter Fritsche <wfritsche95@protonmail.com>
2022-08-10 14:18:06 -0500

    base for product detail subroute component

---
+++ src/components/ProductDetail.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./ProductDetail.css";
+
+
+export default function ProductDetail() {
+  return (
+    <div className='prod-detail-wrapper'>
+      <div className='prod-detail-content'>
+        <p>Product</p>
+      </div>
+    </div>
+  )
+}
