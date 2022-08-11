43d1af1b5c6eb1b9bd43eea9524bd22839023416
Walter Fritsche <wfritsche95@protonmail.com>
2022-08-11 14:18:13 -0500

    added better layout html and state and event handlers for quantity change

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./ProductDetail.css";
 import { useParams, useNavigate } from 'react-router-dom';
 import data from "../data/trees.json";
@@ -8,6 +8,17 @@ export default function ProductDetail() {
   let navigate = useNavigate();
   let params = useParams();
   let tree = data.find(tree => tree.id === parseInt(params.productId as string));
+  const [quantity, setQuantity] = useState(1);
+
+  const quantityChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (parseInt(e.target.value) < 1) {
+      setQuantity(1);
+    } else if (parseInt(e.target.value) > 10) {
+      setQuantity(10);
+    } else {
+      setQuantity(parseInt(e.target.value));
+    }
+  };
 
   return (
     <div className='prod-detail-wrapper' onClick={(e: React.MouseEvent<HTMLDivElement>) => {
@@ -18,12 +29,21 @@ export default function ProductDetail() {
       <div className='prod-detail-content'>
         <img src={tree?.img} alt={tree?.desc}/>
         <div className='prod-detail-description'>
-          <p className='tree-title'>{tree?.name}</p>
-          <ul>
-            <li>{tree?.desc}</li>
-            <li>{tree?.leaf}</li>
-          </ul>
-          <p>Price: {tree?.price}</p>
+          <div>
+            <p className='tree-title'>{tree?.name}</p>
+            <ul>
+              <li>{tree?.desc}</li>
+              <li>{tree?.leaf}</li>
+            </ul>
+          </div>
+          <div className='prod-detail-desc-price-container'>
+            <input type='number' value={quantity} onChange={quantityChangeHandler}/>
+            <p>${tree?.price}</p>
+          </div>
+          <div className='prod-detail-desc-buttons'>
+            <button>Add To Cart</button>
+            <button>Buy Now</button>
+          </div>
         </div>
       </div>
     </div>
