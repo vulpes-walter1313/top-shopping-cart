d723dfd4fd2d4940b58f14b70d95e25fe25b1ecd
Walter Fritsche <wfritsche95@protonmail.com>
2022-08-24 11:41:37 -0500

    App will now hold cartData state and provide data nd dispatch context to nested components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import "./App.css";
 import Layout from "./components/Layout";
-import { Outlet } from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
+import useCartLocalStorage from "./hooks/useCartLocalStorage";
+import type { AddToCartAction, CartData } from "./hooks/useCartLocalStorage";
+
+type ContextType = [cartData: CartData, cartDispatch: React.Dispatch<AddToCartAction>]
 
 function App() {
+  const [cartData, cartDispatch] = useCartLocalStorage();
   return (
-    <Layout>
-      <Outlet />
+    <Layout cartItems={cartData}>
+      <Outlet context={[cartData, cartDispatch]}/>
     </Layout>
   );
+}
+
+export function useCartContext() {
+  return useOutletContext<ContextType>();
 }
 
 export default App;
